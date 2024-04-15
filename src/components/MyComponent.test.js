fdd7eebfd8452a1841eec599d1adbfd92025eb77
Ann Maria Jose <annmariajose1412@gmail.com>
2024-04-15 16:18:29 -0500

    Add final test - onchange

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -13,4 +13,28 @@ describe("MyComponent", () => {
     //then
     expect(component.getElements()).toMatchSnapshot();
   });
+  it("should create an entry in component state", () => {
+    //given
+    const component = shallow(<MyComponent />);
+    const form = component.find('input');
+    //when
+    form.props().onChange({target: {
+      name: 'myName',
+      value: 'myValue'
+    }});
+    //then
+    expect(component.state('input')).toBeDefined();
+  });
+  it("should create an entry in component state with the event value", () => {
+    //given
+    const component = shallow(<MyComponent />);
+    const form = component.find('input');
+    //when
+    form.props().onChange({target: {
+      name: 'myName',
+      value: 'myValue'
+    }});
+    //then
+    expect(component.state('input')).toEqual('myValue');
+  });
 });
